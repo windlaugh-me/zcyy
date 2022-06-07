fc120fcb39452f7d1e3f89a2c8e2fc3a44730aa3
赵春艳 <1874134696@qq.com>
2022-06-07 17:33:39 +0800

    Initial commit

---
+++ src/com/java/HelloWord.java
@@ -0,0 +1,84 @@
+package com.java;
+
+import javax.print.StreamPrintServiceFactory;
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+
+public class HelloWord {
+    private static ArrayList<Object> list12;
+    private static ArrayList<Object> 2;
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < ; i++) {
+            int s=10;
+            System.out.println(s);
+            System.out.println("args = " + Arrays.deepToString(args));
+            System.out.println("HelloWord.main");
+            System.out.println("s = " + s);
+            for (String arg : args) {
+                
+            }
+            for (int j = 0; j < args.length; j++) {
+                String arg = args[j];
+                
+            }
+            ArrayList list=new ArrayList();
+            list.add(123);
+            list.add(456);
+            list.add(789);
+            for (Object o : list) {
+                
+            }
+            for (int i1 = 0; i1 < list.size(); i1++) {
+
+            }
+            for (int i1 = list.size() - 1; i1 >= 0; i1--) {
+                
+            }
+            if (list == null) {
+                
+            }
+            if (list != null) {
+                
+            }
+            if (list != null) {
+
+            }
+
+
+        }
+    }
+public static final String
+ public static final
+
+    public static final int
+
+    /**
+     *
+     *
+     */
+    /**
+     *
+     */
+
+
+
+
+    /*public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("ZCYY!");
+        ArrayList<Object> list = new ArrayList<>();
+        System.out.println();
+
+
+        list12 = new ArrayList<>();
+        Date date = new Date();
+        list12.add();
+        public static  final  int abcv=10*/
+
+
+    }
+}
